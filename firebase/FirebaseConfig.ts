f3868f33935c1389a78e8e8edd112514ac53317a
amoham23-b <amoham23@ku.edu>
2025-04-05 15:02:43 -0500

    .gitignore added

--- firebase/FirebaseConfig.ts
+++ firebase/FirebaseConfig.ts
@@ -1,7 +1,10 @@
-// Import the functions you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
-// Your Firebase config using env variables
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -9,9 +12,20 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+let analytics = null;
 
+// Only initialize analytics on client-side
+if (typeof window !== 'undefined') {
+  analytics = getAnalytics(app);
+}
+
+// Initialize Firebase Authentication and export it
+export const auth = getAuth(app);
+
+// Export app as default
 export default app;
