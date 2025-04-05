4911597673f7dae48609650fe70e5b741566286c
amoham23-b <amoham23@ku.edu>
2025-04-05 14:36:45 -0500

    login auth

--- firebase/FirebaseConfig.ts
+++ firebase/FirebaseConfig.ts
@@ -1,16 +1,17 @@
-
+// Import the functions you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
+// Your Firebase config using env variables
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "linkedout-a983b.firebaseapp.com",
-  projectId: "linkedout-a983b",
-  storageBucket: "linkedout-a983b.firebasestorage.app",
-  messagingSenderId: "864683097998",
-  appId: "1:864683097998:web:66f99fd8b9cd7c2edbeedf",
-  measurementId: "G-BY7NEXGR1Q"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export default app;
